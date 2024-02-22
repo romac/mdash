fd70eb1d3a730ce9f2555a04e241b1e4877b6593
Romain Ruetschi <106849+romac@users.noreply.github.com>
2024-02-22 14:13:33 +0100

    Add Gleam subreddit

--- bookmarks.js
+++ bookmarks.js
@@ -37,7 +37,7 @@ const bookmarks = [
       },
       {
         name: 'Reddit',
-        url: 'https://old.reddit.com/r/haskell+purescript+rust+scala/',
+        url: 'https://old.reddit.com/r/haskell+purescript+rust+scala+gleamlang/',
         favicon: 'img/favicons_035.png'
       },
       {
